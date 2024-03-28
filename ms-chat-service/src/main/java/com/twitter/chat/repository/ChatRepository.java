4fdfbde611ec555d478f7b82162787e7460994f1
Bùi Nguyên Quốc Bảo <quocbao10122001@gmail.com>
2024-03-29 01:27:41 +0700

    US008: fixed getChatByParticipants query

--- ms-chat-service/src/main/java/com/twitter/chat/repository/ChatRepository.java
+++ ms-chat-service/src/main/java/com/twitter/chat/repository/ChatRepository.java
@@ -2,9 +2,7 @@ package com.twitter.chat.repository;
 
 import com.twitter.chat.model.Chat;
 import com.twitter.chat.repository.projection.ChatProjection;
-import dto.response.ChatResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -19,8 +17,8 @@ public interface ChatRepository extends JpaRepository<Chat, Long> {
     ChatProjection getChatById(@Param("chatId") Long chatId);
 
     @Query("SELECT chat FROM Chat chat " +
-            "LEFT JOIN ChatParticipant cp1 " +
-            "LEFT JOIN ChatParticipant cp2 " +
-            " WHERE cp1.userId = :authUserId AND cp2.userId = :userId AND cp1.chat_id = cp2.chat_id")
+            "LEFT JOIN chat.chatParticipants cp1 " +
+            "LEFT JOIN chat.chatParticipants cp2 " +
+            " WHERE cp1.userId = :authUserId AND cp2.userId = :userId AND cp1.id = cp2.id")
     public Optional<Chat> getChatByParticipants(@Param("authUserId") Long authUserId, @Param("userId") Long userId);
 }
